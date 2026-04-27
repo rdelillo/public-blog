55125005462b1cc518a4e5bdd8e49e37e3c7ac64
Robin De Lillo <robin@ynput.io>
2026-04-26 20:54:53 -0400

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://djsiddz.github.io",
-  base: "/space-ahead",
+  site: "https://public-blog-alpha.vercel.app",
+  base: "/",
   integrations: [
     swup({
       theme: ["overlay", { direction: "to-top" }],
