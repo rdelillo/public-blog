56d965723e13806ab6ca1111d085ac54c4c66d5b
robin@ynput.io <robin@ynput.io>
2026-04-26 22:38:12 -0400

    Adjust main config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://public-blog-git-main-rdelillos-projects.vercel.app/",
+  site: "https://public-blog-git-main-rdelillos-projects.vercel.app",
   base: "https://public-blog-git-main-rdelillos-projects.vercel.app/",
   integrations: [
     swup({
--- src/site.config.ts
+++ src/site.config.ts
@@ -96,7 +96,7 @@ const siteConfig: SiteConfig = {
     hero: {
         eyebrowText: '',
         title: 'Hello There ✨',
-        text: "",
+        text: "I’m a VFX developer passionate about image crafting and storytelling. \nI’m just documenting my journey at my own pace, strictly for the fun of it.\n\nIf you find something here that entertains you or makes you smile, that’s a win.\nIf not, at least I’ll know where I left my notes!.",
         image: {
             src: '/assets/images/pixeltrue-space-discovery.svg',
             alt: 'A person sitting at a desk in front of a computer'
@@ -104,12 +104,12 @@ const siteConfig: SiteConfig = {
         actions: [
             {
                 text: 'LinkedIn',
-                href: "https://www.linkedin.com/in/robin-de-lillo-23954342/"
+                href: "https://www.linkedin.com/in/robin-de-lillo-23954342"
             },
             {
                 text: 'GitHub',
                 href: 'https://github.com/rdelillo'
-              }
+            }
         ]
     },
     about: {
