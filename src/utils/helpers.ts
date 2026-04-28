3f629062c414b6c2d0fb516ceb9cfa05c2ce4165
Robin DE LILLO <robindelillo@gmail.com>
2026-04-27 21:18:02 -0400

    Fix withBase

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -32,4 +32,4 @@ export function getPostsByTag(posts: CollectionEntry<'blogs'>[], tagId: string) 
     return filteredPosts;
 }
 
-export const withBase = (path: string) => `${import.meta.env.BASE_URL}${path}`;
+export const withBase = (path: string) => `${import.meta.env.BASE_URL.replace(/\/$/, '')}${path}`;
